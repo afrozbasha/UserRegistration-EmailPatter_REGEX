9378255b86b0eb5f7d0672950fb54f3621d155b6
afrozbasha <iafrozbasha@gmail.com>
2021-08-02 15:23:57 +0530

    User Registration Problem and Email Patter

--- UserRegistration.java
+++ UserRegistration.java
@@ -27,6 +27,19 @@ public class UserRegistration {
             System.out.println("Please Enter a Valid Last Name with Only \"One Starting Capital\" latter");
     }
 
+    public void email(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Email of User: " );
+        String  eMail = sc.next();
+        boolean check = Pattern.matches("^[a-z]*([.]?[a-z]+)*@bl[.]co([.]?in)*", eMail);
+        if (check)
+            System.out.println("Perfect! #Email!");
+        else
+            System.out.println("Please Enter a Valid Email with Only \"example.abc@bl.co.in\" latter");
+    }
+
+
+  
 
 
     public static void main(String[] args) {
@@ -37,6 +50,7 @@ public class UserRegistration {
 
         user.firstNameCheck();
         user.lastNameCheck();
+        user.email();
         
 
 
