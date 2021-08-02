0484e6b9392c85ea8402df74faade3f5748cd502
afrozbasha <iafrozbasha@gmail.com>
2021-08-02 15:30:21 +0530

    User Registration Problem and Email Patter

--- UserRegistration.java
+++ UserRegistration.java
@@ -62,7 +62,20 @@ public class UserRegistration {
             System.out.println("Please Enter a Valid password, it should have minimum 8 characters");
     }
 
-    
+    // rule2  minimum 8 characters with 1 upper case
+    public void passWordRule2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter password :  ");
+        String passWord = sc.nextLine();
+        boolean check = Pattern.matches("[A-Z]{1}[a-z]{7,}", passWord);
+        if (check)
+            System.out.println("Perfect! #password");
+        else
+            System.out.println("Please Enter a Valid password, it should have minimum 8 characters with 1 upper case");
+    }
+
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         UserRegistration user = new UserRegistration();
@@ -74,6 +87,7 @@ public class UserRegistration {
         user.email();
         user.mobileNum();
         user.passWordRule1();
+        user.passWordRule2();
         
 
     }
